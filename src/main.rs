0a4b7f09d8da94ac6a6fa7356a55512778f6cbb0
KaiStaud <kai-staud@web.de>
2023-04-23 17:01:48 +0200

    first test ok

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,151 @@
 use std::u8;
+extern crate spidev;
+extern crate gpio_cdev;
+use std::{thread,time};
+use std::io;
+use std::io::prelude::*;
+use spidev::{Spidev, SpidevOptions, SpidevTransfer, SpiModeFlags};
+use gpio_cdev::{Line,Chip, LineRequestFlags};
+
+fn create_spi() -> io::Result<Spidev> {
+    let mut spi = Spidev::open("/dev/spidev6.0")?;
+    let options = SpidevOptions::new()
+         .bits_per_word(8)
+         .max_speed_hz(20_000)
+         .mode(SpiModeFlags::SPI_MODE_0)
+         .build();
+    spi.configure(&options)?;
+    Ok(spi)
+}
 
 pub struct St7920Glcd{
+spi : Spidev,
+cs : Line,
+graphic : [u8;1042]
 // fb1: [[u8; 128]; 64],
 // fb2: [[u8; 128]; 64]
-
 }
 
 impl St7920Glcd {
-    
-pub fn new(&self) -> Self{
-    St7920Glcd { }
-}    
 
-fn send_cmd(&self,cmd:u8) {
-    
-	/*
-    HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);  // PUll the CS high
-
-	SendByteSPI(0xf8+(0<<1));  // send the SYNC + RS(0)
-	SendByteSPI(cmd&0xf0);  // send the higher nibble first
-	SendByteSPI((cmd<<4)&0xf0);  // send the lower nibble
-	//delay_us(50);
-	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);  // PUll the CS LOW
-*/
+fn delay(&self,us:u64){
+    let timespan = time::Duration::from_micros(us);
+    thread::sleep(timespan);
 }
 
-fn send_data(&self,data:u8){
-/*/	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);  // PUll the CS high
-
-	SendByteSPI(0xf8+(1<<1));  // send the SYNC + RS(1)
-	SendByteSPI(data&0xf0);  // send the higher nibble first
-	SendByteSPI((data<<4)&0xf0);  // send the lower nibble
-	//delay_us(50);
-	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);  // PUll the CS LOW
-*/
+fn write_pin(&mut self,set:bool){
+self.cs.request(LineRequestFlags::OUTPUT,set as u8,"blinky").unwrap();
 }
 
-fn init(&self){
-//    HAL_GPIO_WritePin(RST_PORT, RST_PIN, GPIO_PIN_RESET);  // RESET=0
-	//HAL_Delay(10);   // wait for 10ms
-//	HAL_GPIO_WritePin(RST_PORT, RST_PIN, GPIO_PIN_SET);  // RESET=1
-//HAL_Delay(50);   //wait for >40 ms
+fn send_cmd(&mut self,cmd:u8) {
+
+   self.cs.request(LineRequestFlags::OUTPUT, 1, "blinky").unwrap();
+   self.spi.write(&[0xf8+(0<<1)]);
+   self.spi.write(&[cmd&0xf0]);       // send the higher nibble first
+   self.spi.write(&[(cmd<<4)&0xf0]);  // send the lower nibble
+   self.delay(50);
+   self.cs.request(LineRequestFlags::OUTPUT, 0, "blinky").unwrap();
+}
+
+fn send_data(&mut self,data:u8){
+	self.write_pin(true);  // PUll the CS high
+	self.spi.write(&[0xf8+(1<<1)]);  // send the SYNC + RS(1)
+	self.spi.write(&[data&0xf0]);  // send the higher nibble first
+	self.spi.write(&[(data<<4)&0xf0]);  // send the lower nibble
+	self.delay(50);
+	self.write_pin(false);  // PUll the CS LOW
+
+}
+
+fn init(&mut self){
+    self.write_pin(false);  // RESET=0
+	self.delay(10000);   // wait for 10ms
+    self.write_pin(true);  // RESET=1
+    self.delay(50000);   //wait for >40 ms
 	self.send_cmd(0x30);  // 8bit mode
-	//delay_us(110);  //  >100us delay
+	self.delay(110);  //  >100us delay
 	self.send_cmd(0x30);  // 8bit mode
-	//delay_us(40);  // >37us delay
+	self.delay(40);  // >37us delay
 	self.send_cmd(0x08);  // D=0, C=0, B=0
-	//delay_us(110);  // >100us delay
+	self.delay(110);  // >100us delay
 	self.send_cmd(0x01);  // clear screen
-//HAL_Delay(12);  // >10 ms delay
+    self.delay(12000);  // >10 ms delay
 	self.send_cmd(0x06);  // cursor increment right no shift
-	//HAL_Delay(1);  // 1ms delay
+	self.delay(1000);  // 1ms delay
 	self.send_cmd(0x0C);  // D=1, C=0, B=0
-    //HAL_Delay(1);  // 1ms delay
+    self.delay(1000);  // 1ms delay
 	self.send_cmd(0x02);  // return to home
-	//HAL_Delay(1);  // 1ms delay
+    self.delay(1000);  // 1ms delay
 }
 
-pub fn to_graphic_mode(&self){
+pub fn to_graphic_mode(&mut self){
     self.send_cmd(0x30);  // 8 bit mode
-    //HAL_Delay (1);
+    self.delay (1000);
     self.send_cmd(0x34);  // switch to Extended instructions
-    //HAL_Delay (1);
+    self.delay (1000);
     self.send_cmd(0x36);  // enable graphics
-    //HAL_Delay (1);
+    self.delay (1000);
 }
 
-fn to_normal_mode(&self){
+fn to_normal_mode(&mut self){
 
 }
 
-pub fn set_pixel(&self,x:i32,y:i32,set:bool)->bool{
-    true
+pub fn set_pixel(&mut self,x:i32,y:i32,set:bool)->bool{
+        true
+}
+
+pub fn draw_fb(&mut self){
+	for y in 0..64
+	{
+		if y <32 
+		{
+			for x in 0..8	// Draws top half of the screen.
+			{			
+                // In extended instruction mode, vertical and horizontal coordinates must be specified before sending data in.
+				self.send_cmd(0x80 | y);	// Vertical coordinate of the screen is specified first. (0-31)
+				self.send_cmd(0x80 | x);	// Then horizontal coordinate of the screen is specified. (0-8)
+				self.send_data(0x00);//self.graphic[(2*x + 16*y) as usize]);	// Data to the upper byte is sent to the coordinate.
+				self.send_data(0x00);//self.graphic[(2*x+1 + 16*y) as usize]);	// Data to the lower byte is sent to the coordinate.
+			}
+		}
+		else
+		{
+			for x in 0..8	// Draws bottom half of the screen.
+			{
+              // Actions performed as same as the upper half screen.
+			  self.send_cmd(0x80 | (y-32));// Vertical coordinate must be scaled back to 0-31 as it is dealing with another half of the screen.
+			  self.send_cmd(0x88 | x);
+			  self.send_data(0xFF);//self.graphic[(2*x + 16*y) as usize]);
+			  self.send_data(0xFF);//self.graphic[(2*x+1 + 16*y)as usize]);
+			}
+        }
+    }
 }
 
 }
+
 fn main() {
     println!("Hello, world!");
-    let glcd = St7920Glcd{
-
+    let mut buf : [u8;1042] = [0xFF;1042];
+    let spi6 = create_spi().unwrap();
+    let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+    let handle = chip
+        .get_line(16).unwrap();
+    let mut glcd = St7920Glcd{
+        spi: spi6,
+        cs: handle,
+        graphic: buf,
     };
     glcd.init();
     glcd.to_graphic_mode();
     glcd.set_pixel(0,0,true);
+    glcd.draw_fb();
+for i in glcd.graphic
+    {
+            glcd.graphic[i as usize] = 0x00;
+    }
+    glcd.delay(2000000);
+    glcd.draw_fb();
+
 }
